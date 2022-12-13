6ca6f03fc8f097bf89af3178c8ec607358d382a6
gieghub <120536959+gieghub@users.noreply.github.com>
2022-12-13 20:28:42 +0700

    first commit

--- application/models/Model.php
+++ application/models/Model.php
@@ -4,4 +4,13 @@ class Model extends CI_Model{
     public function simpan($tabel, $data){
         return $this->db->insert($tabel, $data);
     }
+    public function hapus($tabel, $where){
+        return $this->db->idelete($tabel, $where);
+    }
+    public function update($tabel, $data, $where){
+        return $this->db->insert($tabel, $data, $where);
+    }
+    public function getuser($where = ''){
+        return $this->db->query("SELECT * FROM user");
+    }
 }
